913e6a7c7f5d2499a0fcdd9039d76af14eedd566
General <general@sujan.local>
2018-02-27 07:32:04 +0600

    Initial Commit

---
+++ localHtml/ViewController.swift
@@ -0,0 +1,26 @@
+
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let htmlPath = Bundle.main.path(forResource: "index", ofType: "html")
+        let url = URL(fileURLWithPath: htmlPath!)
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
